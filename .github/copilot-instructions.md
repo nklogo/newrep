175675d715c9a78d8feea1557d5e34811f3ee432
Nandhakishor <88738615+nklogo@users.noreply.github.com>
2026-07-13 23:29:28 +0530

    script

---
+++ .github/copilot-instructions.md
@@ -0,0 +1,8 @@
+<!-- UNITY CODE ASSIST INSTRUCTIONS START -->
+- Project name: newrep
+- Unity version: Unity 6000.5.3f1
+- Active game object:
+  - Name: Cube
+  - Tag: Untagged
+  - Layer: Default
+<!-- UNITY CODE ASSIST INSTRUCTIONS END -->
---
+++ Assets/Script/loop.cs
@@ -0,0 +1,23 @@
+using System;
+using UnityEngine;
+
+
+public class loop : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+    void OnMouseDown()
+    {
+        Debug.Log("hellow world");
+    }
+}
